e136c7a57c4d0728b58624a449e2a794dec45a46
Mehul Agarwal <mehulagarwal@Mehuls-MacBook-Pro.local>
2022-12-30 18:48:28 +0530

    Change Config Errors. Update Packages

--- src/CONFIG.mjs
+++ src/CONFIG.mjs
@@ -2,10 +2,10 @@ const {
   OTP_ENABLED = 'false',
   OTP_REDIS_AUTH_ENABLED = 'false',
   OTP_REDIS_CHECK_SERVER_IDENTITY = 'false',
-  OTP_REDIS_HOST = '',
-  OTP_REDIS_PORT = '',
-  OTP_REDIS_KEY_PREFIX = '',
-  OTP_REDIS_AUTH = '',
+  OTP_REDIS_HOST,
+  OTP_REDIS_PORT,
+  OTP_REDIS_KEY_PREFIX,
+  OTP_REDIS_AUTH,
 
   OTP_LENGTH = '6',
   OTP_EXPIRY_IN_SECS = '300',
@@ -17,78 +17,72 @@ const {
 } = process.env
 
 const ENABLED = OTP_ENABLED === 'true'
-const AUTH_ENABLED = OTP_REDIS_AUTH_ENABLED === 'true'
-const CHECK_SERVER_IDENTITY = OTP_REDIS_CHECK_SERVER_IDENTITY === 'true'
+let REDIS_CONNECTION_CONFIG
 
+const REQUIRED_CONFIG = []
+const MISSING_CONFIGS = []
+const INVALID_CONFIGS = []
+const INT_CONFIGS = {
+  OTP_LENGTH,
+  OTP_EXPIRY_IN_SECS,
+  OTP_GEN_HALT_IN_SECS,
+  OTP_GEN_LIMIT,
+  OTP_GEN_LIMIT_EXPIRY_IN_SEC,
+  OTP_REGEN_LIMIT,
+  OTP_VAL_LIMIT
+}
+
+// Handle Missing Configs
 if (ENABLED) {
-  let REQUIRED_CONFIG = ['OTP_REDIS_HOST', 'OTP_REDIS_PORT']
+  REQUIRED_CONFIG.push(OTP_REDIS_HOST)
+  REQUIRED_CONFIG.push(OTP_REDIS_PORT)
+
+  const AUTH_ENABLED = OTP_REDIS_AUTH_ENABLED === 'true'
+  const CHECK_SERVER_IDENTITY = OTP_REDIS_CHECK_SERVER_IDENTITY === 'true'
 
   if (AUTH_ENABLED) {
-    REQUIRED_CONFIG = REQUIRED_CONFIG.concat(['OTP_REDIS_AUTH'])
+    REQUIRED_CONFIG.push('OTP_REDIS_AUTH')
   }
 
   // Terminate Server if any OTP Configuration is missing
   REQUIRED_CONFIG.forEach(function (key) {
     if (!process.env[key]) {
-      console.error('[Error] Missing OTP Config:', key)
-      return process.exit(1)
+      MISSING_CONFIGS.push(key)
     }
   })
+
+  if (MISSING_CONFIGS.length) {
+    console.error(`Missing OTP Configs: ${MISSING_CONFIGS}`)
+    process.exit(1)
+  }
+
+  // Redis Configuration to required establish connection
+  REDIS_CONNECTION_CONFIG = {
+    host: OTP_REDIS_HOST,
+    port: OTP_REDIS_PORT
+  }
+
+  if (AUTH_ENABLED) {
+    REDIS_CONNECTION_CONFIG.password = OTP_REDIS_AUTH
+  }
+
+  if (CHECK_SERVER_IDENTITY) {
+    REDIS_CONNECTION_CONFIG.tls = { checkServerIdentity: () => undefined }
+  }
 }
 
-// Redis Configuration to required establish connection
-const REDIS_CONNECTION_CONFIG = {
-  host: OTP_REDIS_HOST,
-  port: OTP_REDIS_PORT
-}
+// Handle Invalid Configs
+Object.keys(INT_CONFIGS).forEach(key => {
+  const value = INT_CONFIGS[key]
+  INT_CONFIGS[key] = parseInt(value, 10)
 
-if (AUTH_ENABLED) {
-  REDIS_CONNECTION_CONFIG.password = OTP_REDIS_AUTH
-}
+  if (isNaN(INT_CONFIGS[key])) {
+    INVALID_CONFIGS.push(`Invalid OTP Config ${key}. Must be a valid Number: ${value}`)
+  }
+})
 
-if (CHECK_SERVER_IDENTITY) {
-  REDIS_CONNECTION_CONFIG.tls = { checkServerIdentity: () => undefined }
-}
-
-const OTP_LENGTH_INT = parseInt(OTP_LENGTH, 10)
-if (isNaN(OTP_LENGTH_INT)) {
-  console.error('[Error] OTP Config OTP_LENGTH must be a valid Number:', OTP_LENGTH)
-  process.exit(1)
-}
-
-const OTP_EXPIRY_IN_SECS_INT = parseInt(OTP_EXPIRY_IN_SECS, 10)
-if (isNaN(OTP_EXPIRY_IN_SECS_INT)) {
-  console.error('[Error] OTP Config OTP_EXPIRY_IN_SECS must be a valid Number:', OTP_EXPIRY_IN_SECS)
-  process.exit(1)
-}
-
-const OTP_GEN_HALT_IN_SECS_INT = parseInt(OTP_GEN_HALT_IN_SECS, 10)
-if (isNaN(OTP_GEN_HALT_IN_SECS_INT)) {
-  console.error('[Error] OTP Config OTP_GEN_HALT_IN_SECS must be a valid Number:', OTP_GEN_HALT_IN_SECS)
-  process.exit(1)
-}
-
-const OTP_GEN_LIMIT_INT = parseInt(OTP_GEN_LIMIT, 10)
-if (isNaN(OTP_GEN_LIMIT_INT)) {
-  console.error('[Error] OTP Config OTP_GEN_LIMIT must be a valid Number:', OTP_GEN_LIMIT)
-  process.exit(1)
-}
-
-const OTP_GEN_LIMIT_EXPIRY_IN_SEC_INT = parseInt(OTP_GEN_LIMIT_EXPIRY_IN_SEC, 10)
-if (isNaN(OTP_GEN_LIMIT_EXPIRY_IN_SEC_INT)) {
-  console.error('[Error] OTP Config OTP_GEN_LIMIT_EXPIRY_IN_SEC must be a valid Number:', OTP_GEN_LIMIT_EXPIRY_IN_SEC)
-  process.exit(1)
-}
-
-const OTP_REGEN_LIMIT_INT = parseInt(OTP_REGEN_LIMIT, 10)
-if (isNaN(OTP_REGEN_LIMIT_INT)) {
-  console.error('[Error] OTP Config OTP_REGEN_LIMIT must be a valid Number:', OTP_REGEN_LIMIT)
-  process.exit(1)
-}
-
-const OTP_VAL_LIMIT_INT = parseInt(OTP_VAL_LIMIT, 10)
-if (isNaN(OTP_VAL_LIMIT_INT)) {
-  console.error('[Error] OTP Config OTP_VAL_LIMIT must be a valid Number:', OTP_VAL_LIMIT)
+if (INVALID_CONFIGS.length) {
+  INVALID_CONFIGS.map(console.error)
   process.exit(1)
 }
 
@@ -100,21 +94,14 @@ const CONFIG = {
     KEY_PREFIX: OTP_REDIS_KEY_PREFIX
   },
 
-  PINPOINT_CONFIG: {
-    APPLICATION_ID: OTP_PINPOINT_APPLICATION_ID,
-    SMS_ORIGINATION_NUMBER: OTP_PINPOINT_SMS_ORIGINATION_NUMBER,
-    SMS_SENDER_ID: OTP_PINPOINT_SMS_SENDER_ID,
-    EMAIL_FROM_ADDRESS: OTP_PINPOINT_EMAIL_FROM_ADDRESS
-  },
-
   OTP_CONFIG: {
-    OTP_LENGTH: OTP_LENGTH_INT,
-    OTP_EXPIRY_IN_SECS: OTP_EXPIRY_IN_SECS_INT,
-    OTP_GEN_HALT_IN_SECS: OTP_GEN_HALT_IN_SECS_INT,
-    OTP_GEN_LIMIT: OTP_GEN_LIMIT_INT,
-    OTP_GEN_LIMIT_EXPIRY_IN_SEC: OTP_GEN_LIMIT_EXPIRY_IN_SEC_INT,
-    OTP_REGEN_LIMIT: OTP_REGEN_LIMIT_INT,
-    OTP_VAL_LIMIT: OTP_VAL_LIMIT_INT
+    OTP_LENGTH: INT_CONFIGS.OTP_LENGTH,
+    OTP_EXPIRY_IN_SECS: INT_CONFIGS.OTP_EXPIRY_IN_SECS,
+    OTP_GEN_HALT_IN_SECS: INT_CONFIGS.OTP_GEN_HALT_IN_SECS,
+    OTP_GEN_LIMIT: INT_CONFIGS.OTP_GEN_LIMIT,
+    OTP_GEN_LIMIT_EXPIRY_IN_SEC: INT_CONFIGS.OTP_GEN_LIMIT_EXPIRY_IN_SEC,
+    OTP_REGEN_LIMIT: INT_CONFIGS.OTP_REGEN_LIMIT,
+    OTP_VAL_LIMIT: INT_CONFIGS.OTP_VAL_LIMIT
   }
 }
 
