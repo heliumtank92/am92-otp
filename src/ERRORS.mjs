4ba2124b2a7a7fd410eb60f922a1c10dffe0ffd9
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-01-16 13:07:52 +0530

    Fix Error

--- src/ERRORS.mjs
+++ src/ERRORS.mjs
@@ -1,43 +1,43 @@
 const HOTP_NOT_INITIALIZED_ERROR = {
   message: 'Hotp Not Initialized',
   statusCode: 500,
-  errorCode: 'OtpError::HOTP_NOT_INITIALIZED'
+  errorCode: 'Otp::HOTP_NOT_INITIALIZED'
 }
 
 const INVALID_REGEN_REF_ID_ERROR = {
   message: 'Invalid Regeneration Reference Id',
   statusCode: 400,
-  errorCode: 'OtpError::INVALID_REGEN_REF_ID'
+  errorCode: 'Otp::INVALID_REGEN_REF_ID'
 }
 
 const OTP_EXPIRED_ERROR = {
   message: 'Otp Expired',
   statusCode: 400,
-  errorCode: 'OtpError::OTP_EXPIRED'
+  errorCode: 'Otp::OTP_EXPIRED'
 }
 
 const OTP_REGEN_ON_HALT_ERROR = {
   message: 'Otp Generation On Halt',
   statusCode: 400,
-  errorCode: 'OtpError::OTP_REGEN_ON_HALT'
+  errorCode: 'Otp::OTP_REGEN_ON_HALT'
 }
 
 const OTP_GEN_EXCEEDED_ERROR = {
   message: 'Otp Generation Exceeded',
   statusCode: 400,
-  errorCode: 'OtpError::OTP_GEN_EXCEEDED'
+  errorCode: 'Otp::OTP_GEN_EXCEEDED'
 }
 
 const OTP_REGEN_EXCEEDED_ERROR = {
   message: 'Otp Regeneration Exceeded',
   statusCode: 400,
-  errorCode: 'OtpError::OTP_REGEN_EXCEEDED'
+  errorCode: 'Otp::OTP_REGEN_EXCEEDED'
 }
 
 const OTP_VAL_EXCEEDED_ERROR = {
   message: 'Otp Validation Exceeded',
   statusCode: 400,
-  errorCode: 'OtpError::OTP_VAL_EXCEEDED'
+  errorCode: 'Otp::OTP_VAL_EXCEEDED'
 }
 
 export {
