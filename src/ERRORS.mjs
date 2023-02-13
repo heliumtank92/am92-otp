e7c72acacb86f3b905526b76a0a1e29b598e62cd
Mehul Agarwal <agarwalmehul92@gmail.com>
2023-02-13 18:04:31 +0530

    Fix Clear Keys for GenFor at First Time Generation

--- src/ERRORS.mjs
+++ src/ERRORS.mjs
@@ -1,51 +1,51 @@
-const HOTP_NOT_INITIALIZED_ERROR = {
-  message: 'Hotp Not Initialized',
-  statusCode: 500,
-  errorCode: 'Otp::HOTP_NOT_INITIALIZED'
+const ERRORS = {
+  HOTP_NOT_INITIALIZED_ERROR: {
+    message: 'Hotp Not Initialized',
+    statusCode: 500,
+    errorCode: 'Otp::HOTP_NOT_INITIALIZED'
+  },
+
+  INVALID_REF_ID_ERROR: {
+    message: 'Invalid Reference Id',
+    statusCode: 400,
+    errorCode: 'Otp::INVALID_REF_ID'
+  },
+
+  INVALID_REGEN_REF_ID_ERROR: {
+    message: 'Invalid Regeneration Reference Id',
+    statusCode: 400,
+    errorCode: 'Otp::INVALID_REGEN_REF_ID'
+  },
+
+  OTP_EXPIRED_ERROR: {
+    message: 'Otp Expired',
+    statusCode: 400,
+    errorCode: 'Otp::OTP_EXPIRED'
+  },
+
+  OTP_REGEN_ON_HALT_ERROR: {
+    message: 'Otp Generation On Halt',
+    statusCode: 400,
+    errorCode: 'Otp::OTP_REGEN_ON_HALT'
+  },
+
+  OTP_GEN_EXCEEDED_ERROR: {
+    message: 'Otp Generation Exceeded',
+    statusCode: 400,
+    errorCode: 'Otp::OTP_GEN_EXCEEDED'
+  },
+
+  OTP_REGEN_EXCEEDED_ERROR: {
+    message: 'Otp Regeneration Exceeded',
+    statusCode: 400,
+    errorCode: 'Otp::OTP_REGEN_EXCEEDED'
+  },
+
+  OTP_VAL_EXCEEDED_ERROR: {
+    message: 'Otp Validation Exceeded',
+    statusCode: 400,
+    errorCode: 'Otp::OTP_VAL_EXCEEDED'
+  }
 }
 
-const INVALID_REGEN_REF_ID_ERROR = {
-  message: 'Invalid Regeneration Reference Id',
-  statusCode: 400,
-  errorCode: 'Otp::INVALID_REGEN_REF_ID'
-}
-
-const OTP_EXPIRED_ERROR = {
-  message: 'Otp Expired',
-  statusCode: 400,
-  errorCode: 'Otp::OTP_EXPIRED'
-}
-
-const OTP_REGEN_ON_HALT_ERROR = {
-  message: 'Otp Generation On Halt',
-  statusCode: 400,
-  errorCode: 'Otp::OTP_REGEN_ON_HALT'
-}
-
-const OTP_GEN_EXCEEDED_ERROR = {
-  message: 'Otp Generation Exceeded',
-  statusCode: 400,
-  errorCode: 'Otp::OTP_GEN_EXCEEDED'
-}
-
-const OTP_REGEN_EXCEEDED_ERROR = {
-  message: 'Otp Regeneration Exceeded',
-  statusCode: 400,
-  errorCode: 'Otp::OTP_REGEN_EXCEEDED'
-}
-
-const OTP_VAL_EXCEEDED_ERROR = {
-  message: 'Otp Validation Exceeded',
-  statusCode: 400,
-  errorCode: 'Otp::OTP_VAL_EXCEEDED'
-}
-
-export {
-  HOTP_NOT_INITIALIZED_ERROR,
-  INVALID_REGEN_REF_ID_ERROR,
-  OTP_EXPIRED_ERROR,
-  OTP_REGEN_ON_HALT_ERROR,
-  OTP_GEN_EXCEEDED_ERROR,
-  OTP_REGEN_EXCEEDED_ERROR,
-  OTP_VAL_EXCEEDED_ERROR
-}
+export default ERRORS
